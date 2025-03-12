ee1d76109d4fc565bae828261f2e5ec4996ce6fd
Seiyeon Joo <94334477+jjjooo-it@users.noreply.github.com>
2025-03-12 16:01:38 +0900

    feat: 멤버 엔티티 생성

--- Springboot_SpringSecurity_JWT/src/main/java/com/example/springboot_springsecurity_jwt/entity/Member.java
+++ Springboot_SpringSecurity_JWT/src/main/java/com/example/springboot_springsecurity_jwt/entity/Member.java
@@ -1,25 +1,38 @@
 package com.example.springboot_springsecurity_jwt.entity;
 
+import com.example.springboot_springsecurity_jwt.util.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "member")
 @Getter
-@Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Member {
-
+public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
-    @Column(nullable = false, length = 100)
-    private String password; // 비밀번호
-
     @Column(nullable = false, length = 20)
-    private String name; // 실명
+    private String email;
 
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+
+    // Builder를 이용하면 Service에서 체인 형태로 나타낼 수 있어 가독성이 높아짐
+    // MemberService에서 사용
+    @Builder
+    public Member(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
 }
